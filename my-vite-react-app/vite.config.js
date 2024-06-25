0e9af09dc51169459a1f922f49a75b1e5b6716e5
Kiran-Mahendrakar5 <kiranms.xworkz@gmail.com>
2024-06-25 10:47:00 +0530

    react

---
+++ my-vite-react-app/vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Deploy/',  // Update this with your repository name
+});
